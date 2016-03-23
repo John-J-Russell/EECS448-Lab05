13d18083459082b7d4d4561adaba3ac40bb966ba
John Russell <j362r647@ku.edu>
2016-03-23 08:21:00 -0500

    Prototype CreateUser.php completed, pending testing

--- CreateUser.php
+++ CreateUser.php
@@ -2,5 +2,32 @@
 
 	//stuff goes here
 	
+	$user=$_POST["username"];
+	
+	if($user=="")
+	{
+		echo "Invalid entry, name cannot be blank";
+		exit();
+	}
+	
+	$mysqli = new mysqli("mysql.eecs.ku.edu", "jjrussel", "Critic05", "jjrussel");
+	
+	/* check connection */
+	if ($mysqli->connect_errno) {
+		printf("Connect failed: %s\n", $mysqli->connect_error);
+		exit();
+	}
+
+	$query= "INSERT INTO Users (user_id) VALUES (" . \" $user \" . )";
+	
+	if ( $mysqli -> query($query) == true )
+	{
+		//do things here?
+		echo "User successfully added";
+	}
+	else
+	{
+		echo "User name not valid";
+	}
 	
 ?>
