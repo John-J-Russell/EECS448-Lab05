e842bdc02b9fb7629847a7b829b74a8612cb5ae5
John Russell <j362r647@ku.edu>
2016-03-23 19:53:13 -0500

    Create home page for admin functions, basic php to display all users

---
+++ ViewUsers.php
@@ -0,0 +1,34 @@
+<?php
+
+
+	$mysqli = new mysqli("mysql.eecs.ku.edu", "jjrussel", "Critic05", "jjrussel");
+	
+	
+	/* check connection */
+	if ($mysqli->connect_errno) {
+		//printf("Connect failed: %s\n", $mysqli->connect_error);
+		echo "Could not connect";
+		exit();
+	}
+	
+	
+	//These series of quotes and escaped quotes hurt to look at
+	$query = "SELECT user_id FROM Users ORDER by user_id";
+	
+	if ( $result=$mysqli->query($query) ) //triple equal sign?
+	{
+		while($row=$result->fetch_assoc())
+		{
+			echo "<br> $row[\"" . user_id . "\"]";
+		}
+		$result->free();
+	}
+	else
+	{
+		echo "Didn't work.";
+	}
+	
+	$mysqli->close();
+
+
+?>
