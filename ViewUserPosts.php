8b0d498396d14f902d07268f2a2fdf1be0e25fcb
John Russell <j362r647@ku.edu>
2016-03-24 19:34:11 -0500

    Complete first iteration/attempt at displaying all of selected user's posts

--- ViewUserPosts.php
+++ ViewUserPosts.php
@@ -1,4 +1,36 @@
 <?php
 
+	$author_id = $_POST["selectedUser"];
+
+	
+	$mysqli = new mysqli("mysql.eecs.ku.edu", "jjrussel", "Critic05", "jjrussel");
+	
+	
+	/* check connection */
+	if ($mysqli->connect_errno) {
+		//printf("Connect failed: %s\n", $mysqli->connect_error);
+		echo "Could not connect";
+		exit();
+	}
+	
+	
+	
+	$query = "SELECT post_id, content FROM Posts ORDER by post_id";
+	
+	if ( $result=$mysqli->query($query) ) /
+	{
+		echo "Post id# and content of selected user \"" . $author_id . "\" are: <br>";
+		while($row=$result->fetch_assoc())
+		{
+			echo $row[post_id] . "|" . $row[content] . "<br>";
+		}
+		$result->free();
+	}
+	else
+	{
+		echo "Didn't work.";
+	}
+	
+	$mysqli->close();
 
 ?>
